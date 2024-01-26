36306db3cd15f864a7f12289361f439ee8fde59b
Stanley Ugwu <stanleyugwu2018@gmail.com>
2024-01-26 16:33:37 +0100

    chore: setup app config, navigation,  assets and colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,7 +22,14 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins: require("../assets/fonts/Poppins-Medium.ttf"),
+    AirBnbBold: require("../assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Bd.otf"),
+    AirBnbBlack: require("../assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Bk.otf"),
+    AirBnbBlock: require("../assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Blk.otf"),
+    AirBnbLight: require("../assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Lt.otf"),
+    AirBnbMedium: require("../assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Md.otf"),
+    AirBnbExtraBold: require("../assets/fonts/airbnb-cereal-font/AirbnbCereal_W_Bd.otf"),
     ...FontAwesome.font,
   });
 
@@ -48,10 +55,9 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
     </ThemeProvider>
   );
